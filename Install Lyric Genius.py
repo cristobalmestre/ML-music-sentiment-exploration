c6d508f31426de97656c9bef51f91fc7f7a71582
cristobalmestre <cristobal.mestre@gmail.com>
2025-02-02 16:33:26 -0500

    Added directory structure

--- Install Lyric Genius.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import sys
-
-# Run the pip install command
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'lyricsgenius'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'rauth'])
